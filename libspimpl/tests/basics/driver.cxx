52e21f01de08215692dea915f44cee8d643e222f
flightless-coder <milica.bijelovic@gmail.com>
2025-04-15 11:42:59 +0200

    Initialize package

---
+++ libspimpl/tests/basics/driver.cxx
@@ -0,0 +1,14 @@
+#include <sstream>
+#include <stdexcept>
+
+#include <spimpl/spimpl.h>
+
+#undef NDEBUG
+#include <cassert>
+
+int main ()
+{
+  using namespace std;
+  using namespace spimpl;
+
+}
